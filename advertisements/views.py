8b9a32a6ebc86b8b118ce9e324fb2daa5ef98a3d
thrashches <thrashches@yandex.ru>
2022-02-27 17:24:57 +0300

    Initial commit

---
+++ advertisements/views.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from django_filters import rest_framework as filters
+from .filters import AdvertisementFilter
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return []
