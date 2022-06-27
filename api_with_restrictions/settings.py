e3614992f9b6f38b5879db0987be7cabb81153ad
thrashches <thrashches@yandex.ru>
2022-06-27 21:25:09 +0300

    Добавил celery

--- api_with_restrictions/settings.py
+++ api_with_restrictions/settings.py
@@ -102,16 +102,16 @@ WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'netology_classified_ads',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3'
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'netology_classified_ads',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
